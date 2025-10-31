695cc53a5c5c59b5316ae20e7a0a2eba7e2f702e
LuongBaoLoc <luongbaoloc2014@gmail.com>
2025-10-31 21:17:55 +0700

    Fix permission issues

--- MeTroMap_HCM/frmDatVe.Designer.cs
+++ MeTroMap_HCM/frmDatVe.Designer.cs
@@ -21,65 +21,112 @@
             this.lblGaDen = new System.Windows.Forms.Label();
             this.cboGaDen = new System.Windows.Forms.ComboBox();
             this.lblGia = new System.Windows.Forms.Label();
-            this.txtGia = new System.Windows.Forms.TextBox();
-            this.btnTinhGia = new System.Windows.Forms.Button();
+            this.giaVe = new System.Windows.Forms.TextBox();
             this.btnDatVe = new System.Windows.Forms.Button();
-
+            this.sd = new System.Windows.Forms.Label();
+            this.numSoLuong = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numSoLuong)).BeginInit();
             this.SuspendLayout();
-
+            // 
             // lblTuyen
+            // 
             this.lblTuyen.AutoSize = true;
             this.lblTuyen.Location = new System.Drawing.Point(40, 40);
+            this.lblTuyen.Name = "lblTuyen";
+            this.lblTuyen.Size = new System.Drawing.Size(76, 16);
+            this.lblTuyen.TabIndex = 0;
             this.lblTuyen.Text = "Chọn tuyến:";
-
+            // 
             // cboTuyen
+            // 
             this.cboTuyen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboTuyen.Location = new System.Drawing.Point(140, 37);
-            this.cboTuyen.Width = 200;
+            this.cboTuyen.Location = new System.Drawing.Point(196, 37);
+            this.cboTuyen.Name = "cboTuyen";
+            this.cboTuyen.Size = new System.Drawing.Size(364, 24);
+            this.cboTuyen.TabIndex = 1;
             this.cboTuyen.SelectedIndexChanged += new System.EventHandler(this.cboTuyen_SelectedIndexChanged);
-
+            // 
             // lblGaDi
+            // 
             this.lblGaDi.AutoSize = true;
             this.lblGaDi.Location = new System.Drawing.Point(40, 80);
+            this.lblGaDi.Name = "lblGaDi";
+            this.lblGaDi.Size = new System.Drawing.Size(42, 16);
+            this.lblGaDi.TabIndex = 2;
             this.lblGaDi.Text = "Ga đi:";
-
+            // 
             // cboGaDi
+            // 
             this.cboGaDi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboGaDi.Location = new System.Drawing.Point(140, 77);
-            this.cboGaDi.Width = 200;
-
+            this.cboGaDi.Location = new System.Drawing.Point(196, 77);
+            this.cboGaDi.Name = "cboGaDi";
+            this.cboGaDi.Size = new System.Drawing.Size(364, 24);
+            this.cboGaDi.TabIndex = 3;
+            // 
             // lblGaDen
+            // 
             this.lblGaDen.AutoSize = true;
             this.lblGaDen.Location = new System.Drawing.Point(40, 120);
+            this.lblGaDen.Name = "lblGaDen";
+            this.lblGaDen.Size = new System.Drawing.Size(54, 16);
+            this.lblGaDen.TabIndex = 4;
             this.lblGaDen.Text = "Ga đến:";
-
+            // 
             // cboGaDen
+            // 
             this.cboGaDen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboGaDen.Location = new System.Drawing.Point(140, 117);
-            this.cboGaDen.Width = 200;
-
+            this.cboGaDen.Location = new System.Drawing.Point(196, 117);
+            this.cboGaDen.Name = "cboGaDen";
+            this.cboGaDen.Size = new System.Drawing.Size(364, 24);
+            this.cboGaDen.TabIndex = 5;
+            // 
             // lblGia
+            // 
             this.lblGia.AutoSize = true;
-            this.lblGia.Location = new System.Drawing.Point(40, 160);
+            this.lblGia.Location = new System.Drawing.Point(40, 186);
+            this.lblGia.Name = "lblGia";
+            this.lblGia.Size = new System.Drawing.Size(88, 16);
+            this.lblGia.TabIndex = 6;
             this.lblGia.Text = "Giá vé (VNĐ):";
-
-            // txtGia
-            this.txtGia.Location = new System.Drawing.Point(140, 157);
-            this.txtGia.ReadOnly = true;
-            this.txtGia.Width = 200;
-
-            // btnTinhGia
-            this.btnTinhGia.Text = "Tính giá vé";
-            this.btnTinhGia.Location = new System.Drawing.Point(140, 200);
-            this.btnTinhGia.Click += new System.EventHandler(this.btnTinhGia_Click);
-
+            // 
+            // giaVe
+            // 
+            this.giaVe.Location = new System.Drawing.Point(196, 183);
+            this.giaVe.Name = "giaVe";
+            this.giaVe.ReadOnly = true;
+            this.giaVe.Size = new System.Drawing.Size(185, 22);
+            this.giaVe.TabIndex = 7;
+            // 
             // btnDatVe
+            // 
+            this.btnDatVe.Location = new System.Drawing.Point(444, 159);
+            this.btnDatVe.Name = "btnDatVe";
+            this.btnDatVe.Size = new System.Drawing.Size(116, 43);
+            this.btnDatVe.TabIndex = 9;
             this.btnDatVe.Text = "Đặt vé";
-            this.btnDatVe.Location = new System.Drawing.Point(250, 200);
-            this.btnDatVe.Click += new System.EventHandler(this.btnDatVe_Click);
-
+            this.btnDatVe.Click += new System.EventHandler(this.btnThanhToan_Click);
+            // 
+            // sd
+            // 
+            this.sd.AutoSize = true;
+            this.sd.Location = new System.Drawing.Point(40, 151);
+            this.sd.Name = "sd";
+            this.sd.Size = new System.Drawing.Size(81, 16);
+            this.sd.TabIndex = 10;
+            this.sd.Text = "Số lượng vé:";
+            // 
+            // numSoLuong
+            // 
+            this.numSoLuong.Location = new System.Drawing.Point(196, 151);
+            this.numSoLuong.Name = "numSoLuong";
+            this.numSoLuong.Size = new System.Drawing.Size(60, 22);
+            this.numSoLuong.TabIndex = 11;
+            // 
             // frmDatVe
-            this.ClientSize = new System.Drawing.Size(400, 270);
+            // 
+            this.ClientSize = new System.Drawing.Size(589, 234);
+            this.Controls.Add(this.numSoLuong);
+            this.Controls.Add(this.sd);
             this.Controls.Add(this.lblTuyen);
             this.Controls.Add(this.cboTuyen);
             this.Controls.Add(this.lblGaDi);
@@ -87,14 +134,15 @@
             this.Controls.Add(this.lblGaDen);
             this.Controls.Add(this.cboGaDen);
             this.Controls.Add(this.lblGia);
-            this.Controls.Add(this.txtGia);
-            this.Controls.Add(this.btnTinhGia);
+            this.Controls.Add(this.giaVe);
             this.Controls.Add(this.btnDatVe);
+            this.Name = "frmDatVe";
             this.Text = "Đặt vé Metro";
             this.Load += new System.EventHandler(this.frmDatVe_Load);
-
+            ((System.ComponentModel.ISupportInitialize)(this.numSoLuong)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
@@ -106,8 +154,9 @@
         private System.Windows.Forms.Label lblGaDen;
         private System.Windows.Forms.ComboBox cboGaDen;
         private System.Windows.Forms.Label lblGia;
-        private System.Windows.Forms.TextBox txtGia;
-        private System.Windows.Forms.Button btnTinhGia;
+        private System.Windows.Forms.TextBox giaVe;
         private System.Windows.Forms.Button btnDatVe;
+        private System.Windows.Forms.Label sd;
+        private System.Windows.Forms.NumericUpDown numSoLuong;
     }
 }
---
+++ MeTroMap_HCM/frmThanhToan.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace MeTroMap_HCM
+{
+    public partial class frmThanhToan : Form
+    {
+
+        private readonly string _maVe;
+        private readonly string _gaDi;
+        private readonly string _gaDen;
+        private readonly int _soLuong;
+        private readonly double _giaVe;
+        private int _thoiGianConLai = 300; // 5 phút = 300 giây
+        private Timer timerDemNguoc;
+
+        // ✅ Truyền giá vé từ frmDatVe
+        public frmThanhToan(string maVe, string gaDi, string gaDen, int soLuong, double giaVe)
+        {
+            InitializeComponent();
+
+            _maVe = maVe;
+            _gaDi = gaDi;
+            _gaDen = gaDen;
+            _soLuong = soLuong;
+            _giaVe = giaVe; // giờ được truyền đúng
+        }
+
+        private void frmThanhToan_Load(object sender, EventArgs e)
+        {
+            // Hiển thị thông tin vé
+            lblMaVe.Text = "Mã vé: " + _maVe;
+            lblGaDi.Text = "Ga đi: " + _gaDi;
+            lblGaDen.Text = "Ga đến: " + _gaDen;
+            lblSoLuong.Text = "Số lượng vé: " + _soLuong;
+            lblGiaVe.Text = "Giá vé: " + _giaVe.ToString("N0") + " VNĐ";
+
+            // === Tạo mã QR ===
+            string qrContent = $"MAVE:{_maVe}\nGA DI:{_gaDi}\nGA DEN:{_gaDen}\nSO LUONG:{_soLuong}\nGIA VE:{_giaVe:N0}";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrData = qrGenerator.CreateQrCode(qrContent, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrData);
+            picQR.Image = qrCode.GetGraphic(6);
+
+            // === Đếm ngược 5 phút ===
+            timerDemNguoc = new Timer();
+            timerDemNguoc.Interval = 1000;
+            timerDemNguoc.Tick += TimerDemNguoc_Tick;
+            timerDemNguoc.Start();
+
+            lblDemNguoc.Text = "⏰ Còn lại: 05:00";
+        }
+
+        private void TimerDemNguoc_Tick(object sender, EventArgs e)
+        {
+            _thoiGianConLai--;
+
+            int minutes = _thoiGianConLai / 60;
+            int seconds = _thoiGianConLai % 60;
+            lblDemNguoc.Text = $"⏰ Còn lại: {minutes:D2}:{seconds:D2}";
+
+            if (_thoiGianConLai <= 0)
+            {
+                timerDemNguoc.Stop();
+                MessageBox.Show("Hết hạn thanh toán! Vé đã bị hủy.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.Close();
+            }
+        }
+
+        private void btnHoanTat_Click(object sender, EventArgs e)
+        {
+            timerDemNguoc.Stop();
+            MessageBox.Show("Thanh toán thành công! Cảm ơn bạn đã sử dụng MetroMap.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+    }
+}
