bebd93d11d56a57e2d36c4819586f514c371b943
lynhuanh059 <lynhuanh@059@gmail.com>
2025-11-02 10:30:04 +0700

    UI: add/update frmThongTinVe.Designer.cs

--- MeTroMap_HCM/frmThongTinVe.Designer.cs
+++ MeTroMap_HCM/frmThongTinVe.Designer.cs
@@ -38,117 +38,158 @@
             this.lblTrangThai = new System.Windows.Forms.Label();
             this.btnGiaHan = new System.Windows.Forms.Button();
             this.btnHuy = new System.Windows.Forms.Button();
+            this.pnlCard = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlCard.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblMaVe
             // 
-            this.lblMaVe.AutoSize = true;
-            this.lblMaVe.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
-            this.lblMaVe.Location = new System.Drawing.Point(40, 20);
+            this.lblMaVe.BackColor = System.Drawing.Color.White;
+            this.lblMaVe.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaVe.Location = new System.Drawing.Point(26, 63);
+            this.lblMaVe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMaVe.Name = "lblMaVe";
-            this.lblMaVe.Size = new System.Drawing.Size(120, 37);
+            this.lblMaVe.Size = new System.Drawing.Size(90, 28);
             this.lblMaVe.TabIndex = 0;
             this.lblMaVe.Text = "Mã vé: -";
             // 
             // lblTuyenDi
             // 
             this.lblTuyenDi.AutoSize = true;
-            this.lblTuyenDi.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblTuyenDi.Location = new System.Drawing.Point(40, 75);
+            this.lblTuyenDi.BackColor = System.Drawing.Color.White;
+            this.lblTuyenDi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTuyenDi.ForeColor = System.Drawing.Color.Black;
+            this.lblTuyenDi.Location = new System.Drawing.Point(26, 109);
+            this.lblTuyenDi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTuyenDi.Name = "lblTuyenDi";
-            this.lblTuyenDi.Size = new System.Drawing.Size(105, 28);
+            this.lblTuyenDi.Size = new System.Drawing.Size(100, 22);
             this.lblTuyenDi.TabIndex = 1;
             this.lblTuyenDi.Text = "Tuyến Đi: -";
+            this.lblTuyenDi.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblTuyenDen
             // 
             this.lblTuyenDen.AutoSize = true;
-            this.lblTuyenDen.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblTuyenDen.Location = new System.Drawing.Point(40, 110);
+            this.lblTuyenDen.BackColor = System.Drawing.Color.White;
+            this.lblTuyenDen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTuyenDen.ForeColor = System.Drawing.Color.Black;
+            this.lblTuyenDen.Location = new System.Drawing.Point(26, 152);
+            this.lblTuyenDen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTuyenDen.Name = "lblTuyenDen";
-            this.lblTuyenDen.Size = new System.Drawing.Size(121, 28);
+            this.lblTuyenDen.Size = new System.Drawing.Size(112, 22);
             this.lblTuyenDen.TabIndex = 2;
             this.lblTuyenDen.Text = "Tuyến Đến: -";
+            this.lblTuyenDen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblGaDi
             // 
             this.lblGaDi.AutoSize = true;
-            this.lblGaDi.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblGaDi.Location = new System.Drawing.Point(40, 145);
+            this.lblGaDi.BackColor = System.Drawing.Color.White;
+            this.lblGaDi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGaDi.ForeColor = System.Drawing.Color.Black;
+            this.lblGaDi.Location = new System.Drawing.Point(26, 194);
+            this.lblGaDi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGaDi.Name = "lblGaDi";
-            this.lblGaDi.Size = new System.Drawing.Size(77, 28);
+            this.lblGaDi.Size = new System.Drawing.Size(76, 22);
             this.lblGaDi.TabIndex = 3;
             this.lblGaDi.Text = "Ga Đi: -";
+            this.lblGaDi.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblGaDen
             // 
             this.lblGaDen.AutoSize = true;
-            this.lblGaDen.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblGaDen.Location = new System.Drawing.Point(40, 180);
+            this.lblGaDen.BackColor = System.Drawing.Color.White;
+            this.lblGaDen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGaDen.ForeColor = System.Drawing.Color.Black;
+            this.lblGaDen.Location = new System.Drawing.Point(26, 236);
+            this.lblGaDen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGaDen.Name = "lblGaDen";
-            this.lblGaDen.Size = new System.Drawing.Size(93, 28);
+            this.lblGaDen.Size = new System.Drawing.Size(88, 22);
             this.lblGaDen.TabIndex = 4;
             this.lblGaDen.Text = "Ga Đến: -";
+            this.lblGaDen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblLoaiVe
             // 
             this.lblLoaiVe.AutoSize = true;
-            this.lblLoaiVe.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblLoaiVe.Location = new System.Drawing.Point(40, 225);
+            this.lblLoaiVe.BackColor = System.Drawing.Color.White;
+            this.lblLoaiVe.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoaiVe.ForeColor = System.Drawing.Color.Black;
+            this.lblLoaiVe.Location = new System.Drawing.Point(26, 278);
+            this.lblLoaiVe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLoaiVe.Name = "lblLoaiVe";
-            this.lblLoaiVe.Size = new System.Drawing.Size(90, 28);
+            this.lblLoaiVe.Size = new System.Drawing.Size(88, 22);
             this.lblLoaiVe.TabIndex = 5;
             this.lblLoaiVe.Text = "Loại vé: -";
+            this.lblLoaiVe.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblGiaVe
             // 
             this.lblGiaVe.AutoSize = true;
-            this.lblGiaVe.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblGiaVe.Location = new System.Drawing.Point(40, 260);
+            this.lblGiaVe.BackColor = System.Drawing.Color.White;
+            this.lblGiaVe.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGiaVe.ForeColor = System.Drawing.Color.Black;
+            this.lblGiaVe.Location = new System.Drawing.Point(26, 317);
+            this.lblGiaVe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGiaVe.Name = "lblGiaVe";
-            this.lblGiaVe.Size = new System.Drawing.Size(83, 28);
+            this.lblGiaVe.Size = new System.Drawing.Size(81, 22);
             this.lblGiaVe.TabIndex = 6;
             this.lblGiaVe.Text = "Giá vé: -";
+            this.lblGiaVe.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblNgayBatDau
             // 
             this.lblNgayBatDau.AutoSize = true;
-            this.lblNgayBatDau.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblNgayBatDau.Location = new System.Drawing.Point(40, 300);
+            this.lblNgayBatDau.BackColor = System.Drawing.Color.White;
+            this.lblNgayBatDau.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNgayBatDau.ForeColor = System.Drawing.Color.Black;
+            this.lblNgayBatDau.Location = new System.Drawing.Point(26, 359);
+            this.lblNgayBatDau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNgayBatDau.Name = "lblNgayBatDau";
-            this.lblNgayBatDau.Size = new System.Drawing.Size(148, 28);
+            this.lblNgayBatDau.Size = new System.Drawing.Size(131, 22);
             this.lblNgayBatDau.TabIndex = 7;
             this.lblNgayBatDau.Text = "Ngày bắt đầu: -";
+            this.lblNgayBatDau.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblNgayHetHan
             // 
             this.lblNgayHetHan.AutoSize = true;
-            this.lblNgayHetHan.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblNgayHetHan.Location = new System.Drawing.Point(40, 340);
+            this.lblNgayHetHan.BackColor = System.Drawing.Color.White;
+            this.lblNgayHetHan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNgayHetHan.ForeColor = System.Drawing.Color.Black;
+            this.lblNgayHetHan.Location = new System.Drawing.Point(26, 403);
+            this.lblNgayHetHan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNgayHetHan.Name = "lblNgayHetHan";
-            this.lblNgayHetHan.Size = new System.Drawing.Size(146, 28);
+            this.lblNgayHetHan.Size = new System.Drawing.Size(129, 22);
             this.lblNgayHetHan.TabIndex = 8;
             this.lblNgayHetHan.Text = "Ngày hết hạn: -";
+            this.lblNgayHetHan.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblTrangThai
             // 
             this.lblTrangThai.AutoSize = true;
-            this.lblTrangThai.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.lblTrangThai.Location = new System.Drawing.Point(40, 380);
+            this.lblTrangThai.BackColor = System.Drawing.Color.White;
+            this.lblTrangThai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTrangThai.Location = new System.Drawing.Point(26, 445);
+            this.lblTrangThai.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTrangThai.Name = "lblTrangThai";
-            this.lblTrangThai.Size = new System.Drawing.Size(127, 28);
+            this.lblTrangThai.Size = new System.Drawing.Size(116, 23);
             this.lblTrangThai.TabIndex = 9;
             this.lblTrangThai.Text = "Trạng thái: -";
             // 
             // btnGiaHan
             // 
-            this.btnGiaHan.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGiaHan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGiaHan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(99)))), ((int)(((byte)(235)))));
+            this.btnGiaHan.FlatAppearance.BorderSize = 0;
             this.btnGiaHan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGiaHan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
             this.btnGiaHan.ForeColor = System.Drawing.Color.White;
-            this.btnGiaHan.Location = new System.Drawing.Point(40, 420);
+            this.btnGiaHan.Location = new System.Drawing.Point(132, 510);
+            this.btnGiaHan.Margin = new System.Windows.Forms.Padding(4);
             this.btnGiaHan.Name = "btnGiaHan";
-            this.btnGiaHan.Size = new System.Drawing.Size(200, 40);
+            this.btnGiaHan.Size = new System.Drawing.Size(204, 55);
             this.btnGiaHan.TabIndex = 10;
             this.btnGiaHan.Text = "Gia hạn vé";
             this.btnGiaHan.UseVisualStyleBackColor = false;
@@ -156,46 +197,79 @@
             // 
             // btnHuy
             // 
-            this.btnHuy.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnHuy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnHuy.BackColor = System.Drawing.Color.White;
+            this.btnHuy.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnHuy.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(165)))), ((int)(((byte)(165)))));
             this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnHuy.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.btnHuy.ForeColor = System.Drawing.Color.White;
-            this.btnHuy.Location = new System.Drawing.Point(288, 420);
+            this.btnHuy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.btnHuy.Location = new System.Drawing.Point(393, 510);
+            this.btnHuy.Margin = new System.Windows.Forms.Padding(4);
             this.btnHuy.Name = "btnHuy";
-            this.btnHuy.Size = new System.Drawing.Size(200, 40);
+            this.btnHuy.Size = new System.Drawing.Size(204, 55);
             this.btnHuy.TabIndex = 11;
             this.btnHuy.Text = "Hủy vé";
             this.btnHuy.UseVisualStyleBackColor = false;
             this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
             // 
+            // pnlCard
+            // 
+            this.pnlCard.BackColor = System.Drawing.Color.White;
+            this.pnlCard.Controls.Add(this.label1);
+            this.pnlCard.Controls.Add(this.btnHuy);
+            this.pnlCard.Controls.Add(this.lblMaVe);
+            this.pnlCard.Controls.Add(this.lblTuyenDi);
+            this.pnlCard.Controls.Add(this.btnGiaHan);
+            this.pnlCard.Controls.Add(this.lblTuyenDen);
+            this.pnlCard.Controls.Add(this.lblTrangThai);
+            this.pnlCard.Controls.Add(this.lblGaDi);
+            this.pnlCard.Controls.Add(this.lblNgayHetHan);
+            this.pnlCard.Controls.Add(this.lblGaDen);
+            this.pnlCard.Controls.Add(this.lblNgayBatDau);
+            this.pnlCard.Controls.Add(this.lblLoaiVe);
+            this.pnlCard.Controls.Add(this.lblGiaVe);
+            this.pnlCard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCard.Location = new System.Drawing.Point(0, 0);
+            this.pnlCard.Name = "pnlCard";
+            this.pnlCard.Size = new System.Drawing.Size(702, 593);
+            this.pnlCard.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.label1.Location = new System.Drawing.Point(252, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 35);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "THÔNG TIN VÉ";
+            // 
             // frmThongTinVe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(550, 500);
-            this.Controls.Add(this.btnHuy);
-            this.Controls.Add(this.btnGiaHan);
-            this.Controls.Add(this.lblTrangThai);
-            this.Controls.Add(this.lblNgayHetHan);
-            this.Controls.Add(this.lblNgayBatDau);
-            this.Controls.Add(this.lblGiaVe);
-            this.Controls.Add(this.lblLoaiVe);
-            this.Controls.Add(this.lblGaDen);
-            this.Controls.Add(this.lblGaDi);
-            this.Controls.Add(this.lblTuyenDen);
-            this.Controls.Add(this.lblTuyenDi);
-            this.Controls.Add(this.lblMaVe);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(242)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(702, 593);
+            this.Controls.Add(this.pnlCard);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(720, 640);
+            this.MinimumSize = new System.Drawing.Size(720, 640);
             this.Name = "frmThongTinVe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thông tin vé";
+            this.pnlCard.ResumeLayout(false);
+            this.pnlCard.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Panel pnlCard;
+        private System.Windows.Forms.Label label1;
     }
 }
