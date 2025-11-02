6208d80e86407de8b8a402b5648dcb0fde32c1dd
lynhuanh059 <lynhuanh@059@gmail.com>
2025-11-02 11:58:44 +0700

    UI: update FormAnhCacTuyen.Designer.cs

--- MeTroMap_HCM/FormAnhCacTuyen.Designer.cs
+++ MeTroMap_HCM/FormAnhCacTuyen.Designer.cs
@@ -8,47 +8,73 @@ namespace MeTroMap_HCM
     {
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTieuDe;
 
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnhCacTuyen));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTieuDe = new System.Windows.Forms.Label();
+            this.pnlCard = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlCard.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = global::MeTroMap_HCM.Properties.Resources._3_ban_do_metro_tphcm_cap_nhat_moi_nhat;
-            this.pictureBox1.Location = new System.Drawing.Point(66, 55);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 16);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(799, 565);
+            this.pictureBox1.Size = new System.Drawing.Size(1370, 569);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // label1
+            // lblTieuDe
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F);
-            this.label1.Location = new System.Drawing.Point(169, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(491, 32);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Ảnh Các Tuyến Metro HCM Tương Lai";
+            this.lblTieuDe.BackColor = System.Drawing.Color.Transparent;
+            this.lblTieuDe.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTieuDe.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTieuDe.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.lblTieuDe.Location = new System.Drawing.Point(0, 0);
+            this.lblTieuDe.Name = "lblTieuDe";
+            this.lblTieuDe.Padding = new System.Windows.Forms.Padding(0, 12, 0, 8);
+            this.lblTieuDe.Size = new System.Drawing.Size(1558, 64);
+            this.lblTieuDe.TabIndex = 1;
+            this.lblTieuDe.Text = "Ảnh Các Tuyến Metro HCM Tương Lai";
+            this.lblTieuDe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlCard
+            // 
+            this.pnlCard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlCard.BackColor = System.Drawing.Color.White;
+            this.pnlCard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCard.Controls.Add(this.pictureBox1);
+            this.pnlCard.Location = new System.Drawing.Point(86, 67);
+            this.pnlCard.Name = "pnlCard";
+            this.pnlCard.Padding = new System.Windows.Forms.Padding(16);
+            this.pnlCard.Size = new System.Drawing.Size(1404, 603);
+            this.pnlCard.TabIndex = 2;
             // 
             // FormAnhCacTuyen
             // 
-            this.ClientSize = new System.Drawing.Size(913, 622);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(242)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1558, 698);
+            this.Controls.Add(this.pnlCard);
+            this.Controls.Add(this.lblTieuDe);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FormAnhCacTuyen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormAnhCacTuyen";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlCard.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -57,5 +83,7 @@ namespace MeTroMap_HCM
             if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
+
+        private Panel pnlCard;
     }
 }
